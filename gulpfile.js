a918a54b19a5822940d216ad2f76d7f9403ec833
Budz-Vit <typesetter22@gmail.com>
2023-03-02 18:32:24 +0200

    added video and ptoducts-mixitup

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,8 @@ function browsersync() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
